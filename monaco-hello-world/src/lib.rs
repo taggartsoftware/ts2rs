0ceeae9e3486761d02d2e65b41aaa73a7fe9193c
Cameron Taggart <cameron.taggart@gmail.com>
2020-05-29 19:17:11 -0600

    commit to reproduce https://github.com/rustwasm/wasm-pack/issues/853

--- monaco-hello-world/src/lib.rs
+++ monaco-hello-world/src/lib.rs
@@ -7,40 +7,41 @@ pub fn main_js() -> Result<(), JsValue> {
     Ok(())
 }
 
-// pub mod monaco {
-//     pub mod editor {
-//         use js_sys::Object;
-//         use wasm_bindgen::{prelude::*, JsCast};
-//         use web_sys::HtmlElement;
-//         type HTMLElement = HtmlElement;
+pub mod monaco {
+    pub mod editor {
+        use js_sys::Object;
+        use wasm_bindgen::{prelude::*, JsCast};
+        use web_sys::HtmlElement;
+        type HTMLElement = HtmlElement;
 
-//         #[wasm_bindgen(module = "monaco", js_namespace = ["monaco", "editor"])]
-//         extern "C" {
+        // #[wasm_bindgen(module = "monaco", js_namespace = ["monaco", "editor"])]
+        #[wasm_bindgen(js_namespace = ["nestedNamespace", "InnerClass"])]
+        extern "C" {
 
-//             pub type IStandaloneEditorConstructionOptions;
+            pub type IStandaloneEditorConstructionOptions;
 
-//             pub type IEditorOverrideServices;
+            pub type IEditorOverrideServices;
 
-//             pub type IStandaloneCodeEditor;
+            pub type IStandaloneCodeEditor;
 
-//             #[wasm_bindgen(method, setter, js_name=value)]
-//             pub fn set_value(this: &IStandaloneEditorConstructionOptions, value: Option<&str>);
+            #[wasm_bindgen(method, setter, js_name=value)]
+            pub fn set_value(this: &IStandaloneEditorConstructionOptions, value: Option<&str>);
 
-//             #[wasm_bindgen(method, setter, js_name=language)]
-//             pub fn set_language(this: &IStandaloneEditorConstructionOptions, value: Option<&str>);
+            #[wasm_bindgen(method, setter, js_name=language)]
+            pub fn set_language(this: &IStandaloneEditorConstructionOptions, value: Option<&str>);
 
-//             // export function create(domElement: HTMLElement, options?: IStandaloneEditorConstructionOptions, override?: IEditorOverrideServices): IStandaloneCodeEditor;
-//             pub fn create(
-//                 dom_elmeent: &HTMLElement,
-//                 options: Option<&IStandaloneEditorConstructionOptions>,
-//                 override_: Option<IEditorOverrideServices>,
-//             ) -> IStandaloneCodeEditor;
-//         }
+            // export function create(domElement: HTMLElement, options?: IStandaloneEditorConstructionOptions, override?: IEditorOverrideServices): IStandaloneCodeEditor;
+            pub fn create(
+                dom_elmeent: &HTMLElement,
+                options: Option<&IStandaloneEditorConstructionOptions>,
+                override_: Option<IEditorOverrideServices>,
+            ) -> IStandaloneCodeEditor;
+        }
 
-//         impl IStandaloneEditorConstructionOptions {
-//             pub fn new() -> Self {
-//                 JsCast::unchecked_into(Object::new())
-//             }
-//         }
-//     }
-// }
+        impl IStandaloneEditorConstructionOptions {
+            pub fn new() -> Self {
+                JsCast::unchecked_into(Object::new())
+            }
+        }
+    }
+}
