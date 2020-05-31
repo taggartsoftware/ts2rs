738c76e466a2052c6aac8792ca7b315a8e670e39
Cameron Taggart <cameron.taggart@gmail.com>
2020-05-30 18:58:35 -0600

    configure Monaco environment in rust

--- monaco-editor-hello-world/src/lib.rs
+++ monaco-editor-hello-world/src/lib.rs
@@ -1,22 +1,52 @@
 use monaco::editor::IStandaloneEditorConstructionOptions;
 use wasm_bindgen::{prelude::*, JsCast, JsValue};
-use web_sys::{console::log_1, HtmlElement};
+use web_sys::HtmlElement;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    log_1(&JsValue::from_str("main_js"));
+    // configure Monaco environment by setting the get_worker_url callback
+    let monaco_environment = monaco::Environment::new();
+    let get_worker_url = move |_worker_id: String, label: String| match label.as_str() {
+        "json" => "./json.worker.js".to_owned(),
+        "css" => "./css.worker.js".to_owned(),
+        "html" => "./html.worker.js".to_owned(),
+        "typescript" | "javascript" => "./ts.worker.js".to_owned(),
+        _ => "./editor.worker.js".to_owned(),
+    };
+    monaco_environment.set_get_worker_url(&Closure::wrap(Box::new(get_worker_url)));
+    let global = js_sys::global();
+    js_sys::Object::define_property(&global, &JsValue::from_str("MonacoEnvironment"), monaco_environment.unchecked_ref());
 
     let window = web_sys::window().expect("window");
     let document = window.document().expect("document");
-    let container: HtmlElement = JsCast::unchecked_into(document.get_element_by_id("container").expect("container"));
+    let container: HtmlElement = document.get_element_by_id("container").expect("container").unchecked_into();
     let options = IStandaloneEditorConstructionOptions::new();
     options.set_value(Some("function x() {\n  console.log(\"Hello world!\");\n}"));
     options.set_language(Some("javascript"));
     monaco::editor::create(&container, Some(&options), None);
+
     Ok(())
 }
 
 pub mod monaco {
+    use js_sys::Object;
+    use wasm_bindgen::{prelude::*, JsCast};
+
+    #[wasm_bindgen()]
+    extern "C" {
+
+        pub type Environment;
+
+        # [ wasm_bindgen ( method , setter , js_name = getWorkerUrl ) ]
+        pub fn set_get_worker_url(this: &Environment, value: &Closure<dyn FnMut(String, String) -> String>);
+    }
+
+    impl Environment {
+        pub fn new() -> Self {
+            Object::new().unchecked_into()
+        }
+    }
+
     pub mod editor {
         use js_sys::Object;
         use wasm_bindgen::{prelude::*, JsCast};
@@ -48,7 +78,7 @@ pub mod monaco {
 
         impl IStandaloneEditorConstructionOptions {
             pub fn new() -> Self {
-                JsCast::unchecked_into(Object::new())
+                Object::new().unchecked_into()
             }
         }
     }
